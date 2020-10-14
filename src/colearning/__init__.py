ddbe3fd4e64189b49491c1c873b466368789302a
Shiplu Hawlader <shiplu.cse.du@gmail.com>
2020-10-14 03:53:51 -0500

    Added a single admin account which is also a instructor

--- src/colearning/__init__.py
+++ src/colearning/__init__.py
@@ -4,12 +4,14 @@ import py4web
 assert py4web.check_compatible("0.1.20190709.1")
 
 # by importing db you expose it to the _dashboard/dbadmin
-from .models import db
+from .models import db, create_admin_account
 
 # by importing controllers you expose the actions defined in it
 from . import controllers
 
 # optional parameters
-__version__ = "0.0.0"
-__author__ = "you <you@example.com>"
-__license__ = "anything you want"
+__version__ = "0.0.1"
+__author__ = "Shiplu <shiplu.cse.du@gmail.com.com>"
+__license__ = "Not decided yet"
+
+msg = create_admin_account(email='mhwlader@memphis.edu', username='mhwlader', first_name='Shiplu', last_name='Hawlader', password='abcd123456')
--- src/colearning/models.py
+++ src/colearning/models.py
@@ -2,7 +2,7 @@
 This file defines the database models
 """
 
-from .common import db, Field
+from .common import db, Field, groups, auth
 from pydal.validators import *
 
 ### Define your table below
@@ -13,3 +13,13 @@ from pydal.validators import *
 #
 # db.commit()
 #
+
+def create_admin_account(email, username, first_name, last_name, password):
+     if db(db.auth_user.email==email).select().first() is not None:
+          return "User already exists."
+     
+     user_id = auth.register({'email': email, 'username': username, 'first_name': first_name, 'last_name': last_name, 'password': password})
+     db(db.auth_user.id==user_id).update(action_token='')
+     groups.add(user_id, ['admin', 'teacher'])
+     db.commit()
+     return "Admin added successfully!"
