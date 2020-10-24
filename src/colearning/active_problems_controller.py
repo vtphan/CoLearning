592eeb7a64c1cf9a3d2672aeb36fde2ec952ea25
Shiplu Hawlader <shiplu.cse.du@gmail.com>
2020-10-24 01:51:24 -0500

    Changed name of active problems page

--- src/colearning/active_problems_controller.py
+++ src/colearning/active_problems_controller.py
@@ -4,9 +4,9 @@ from . import settings
 from py4web.utils.form import Form, FormStyleBulma
 import datetime
 
-@action('active_problem', method='GET')
-@action.uses(auth.user, 'active_problem.html')
-def active_problem():
+@action('active_problems', method='GET')
+@action.uses(auth.user, 'active_problems.html')
+def active_problems():
     # if 'student' not in groups.get(auth.get_user()['id']):
     #     redirect(URL('not_authorized'))
     problems = db(db.problem.deadline>datetime.datetime.now()).select()
