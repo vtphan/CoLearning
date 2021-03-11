a04c04e199e61c23d717265bef605da71b5a96cd
Shiplu <shiplu.cse.du@gmail.com>
2021-03-11 01:15:59 -0600

    Added a page to run database query

---
+++ src/colearning/run_query_controller.py
@@ -0,0 +1,17 @@
+from py4web import action, request, Field,redirect, URL
+from .common import db, groups, auth, flash
+from . import settings
+from py4web.utils.form import Form, FormStyleBulma
+import datetime
+from pydal.validators import IS_IN_SET, IS_NOT_EMPTY
+
+@action('run_query', method='GET')
+@action.uses(auth.user, 'run_query.html')
+def run_query():
+    if 'admin' not in groups.get(auth.get_user()['id']):
+        redirect(URL('unauthorized_access'))
+    query_form = Form([Field('query', type='text')])
+    if query_form.accepted:
+        db.executesql(query_form.vars.query)
+        db.commit()
+    return dict(form=query_form)
