3344e0ceeac9b76ce0fa02ea72898f9ecd7e3a08
Shiplu <shiplu.cse.du@gmail.com>
2021-04-05 00:51:38 -0500

    bug fixed

--- src/colearning/help_message_view_controller.py
+++ src/colearning/help_message_view_controller.py
@@ -1,5 +1,6 @@
 from os import setuid
 from py4web import action, request, Field,redirect, URL
+from py4web.core import user_in
 from .common import db, groups, auth, flash
 from . import settings
 from py4web.utils.form import Form, FormStyleBulma
@@ -11,19 +12,12 @@ from .utils import create_notification, is_eligible_for_help
 @action.uses(auth.user, 'student_help_message_list.html')
 def student_help_message_list():
     user_id = auth.get_user()['id']
-    if ('teacher' or 'ta') in groups.get(user_id):
-        messages = db.executesql("select s.first_name, s.last_name, h.id as message_id, h.student_id, h.problem_id, p.problem_name, h.message, h.status \
-            from help_queue h, problem p, auth_user s where p.id=h.problem_id and s.id=h.student_id and h.status<>\"closed\" order by h.asked_at desc", as_dict=True)
+    messages = db.executesql("select s.first_name, s.last_name, h.id as message_id, h.student_id, h.problem_id, p.problem_name, h.message, h.status \
+        from help_queue h, problem p, auth_user s where p.id=h.problem_id and s.id=h.student_id and h.status<>\"closed\" and \
+            p.id in (select problem_id from submission sb, submission_verdict sv where sb.id=sv.submission_id and sb.student_id="+str(user_id)+") order by h.asked_at desc", as_dict=True)
+    past_messages = []
 
-        past_messages = db.executesql("select s.first_name, s.last_name, h.id as message_id, h.student_id, h.problem_id, p.problem_name, h.message, h.status\
-            from help_queue h, problem p, auth_user s where p.id=h.problem_id and s.id=h.student_id and h.status=\"closed\" order by h.asked_at desc", as_dict=True)
-    elif 'student' in groups.get(user_id):
-        messages = db.executesql("select s.first_name, s.last_name, h.id as message_id, h.student_id, h.problem_id, p.problem_name, h.message, h.status \
-            from help_queue h, problem p, auth_user s where p.id=h.problem_id and s.id=h.student_id and h.status<>\"closed\" and \
-                p.id in (select problem_id from submission sb, submission_verdict sv where sb.id=sv.submission_id and sb.student_id="+str(user_id)+") order by h.asked_at desc", as_dict=True)
-        past_messages = []
-
-    return dict(messages=messages, past_messages=past_messages)
+    return dict(messages=messages, past_messages=past_messages, user_role='student')
 
 @action('help_message_list', method='GET')
 @action.uses(auth.user, 'help_message_list.html')
@@ -33,6 +27,8 @@ def help_message_list():
         user_role = 'instructor'
     elif 'ta' in groups.get(user_id):
         user_role = 'ta'
+    elif 'student' in groups.get(user_id):
+        user_role = 'student'
     else:
         redirect(URL('not_authorized'))
     
