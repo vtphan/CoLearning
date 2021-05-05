6a513d0e1e93761efab588247f60c820377d924c
vtphan <vphan@memphis.edu>
2021-05-05 01:17:25 -0500

    .

--- src/py4web_app/models.py
+++ src/py4web_app/models.py
@@ -6,33 +6,17 @@ from .common import T, db, Field, groups, auth
 from pydal.validators import *
 import datetime
 from pydal.validators import IS_IN_SET
-### Define your table below
-#
-# db.define_table('thing', Field('name'))
-#
-## always commit your models to avoid problems later
-#
-# db.commit()
-#
 
 def create_global_parameter(variable, value):
-     if db(db.global_value.variable==variable).select().first() is not None:
-          return "Variable already exists"
-     db.global_value.insert(variable=variable, value=value)
-     db.commit()
-
-# def create_admin_account(email, username, first_name, last_name, password):
-#      if db(db.auth_user).select().first() is not None:
-#           return "First user exists."
-#      user_id = auth.register({'email': email, 'username': username, 'first_name': first_name, 'last_name': last_name, 'password': password})
-#      db(db.auth_user.id==user_id).update(action_token='')
-#      groups.add(user_id, ['admin', 'teacher'])
-#      db.commit()
-#      return "Admin added successfully!"
+    if db(db.global_value.variable==variable).select().first() is not None:
+        return "Variable already exists"
+    db.global_value.insert(variable=variable, value=value)
+    db.commit()
 
 def create_tables():
-     db.define_table('topic', Field('topic_description', unique=True))
-     db.define_table('problem', 
+    db.define_table('topic', Field('topic_description', unique=True))
+    
+    db.define_table('problem', 
         Field('teacher_id', type='reference auth_user'), 
         Field('problem_name'), 
         Field('problem_description', type='text'), 
@@ -47,33 +31,76 @@ def create_tables():
         Field('published_at', type='datetime'), 
         Field('type', requires=IS_IN_SET("in-class", "homework")),
         Field('last_updated_at', type='datetime'), 
-        redefine=True) 
-     db.define_table('problem_topic', Field('problem_id', type='reference problem'), Field('topic_id', type='reference topic'))
-     db.define_table('student_workspace', Field('problem_id', type='reference problem'), Field('student_id', type='reference auth_user'), \
-          Field('content', type='text'), Field('attempt_left', type='integer'), Field('updated_at', type='datetime', default=datetime.datetime.utcnow()), redefine=True)
-     db.define_table('submission', Field('problem_id', type='reference problem'), Field('student_id', type='reference auth_user'),\
-          Field('content', type='text'), Field('submitted_at', type='datetime', default=datetime.datetime.utcnow()), \
-               Field('submission_category', type='integer', default=1), Field('attempt', type='integer'), redefine=True)
-     
-     db.define_table('submission_verdict', Field('submission_id', type='reference submission'), Field('verdict'), Field('score', type='double'),\
-           Field('evaluated_at', type='datetime'))
+        redefine=True)
 
-     db.define_table('notification', Field('message', type='text'), Field('recipients', type='list:reference auth_user'), Field('generated_at', type='datetime'),\
-          Field('expire_at', type='datetime'), Field('type'), Field('type_id', type='integer'), redefine=True)
-     db.define_table('global_value', Field('variable'), Field('value'), redefine=True)
-     db.define_table('notification_queue', Field('notification_id', type='reference notification'), Field('user_id', type='reference auth_user'))
-     db.define_table('editor_notification_queue', Field('notification_id', type='reference notification'), Field('user_id', type='reference auth_user'))
-     
-     db.define_table('discussion', Field('message', type='text'), Field('code_snapshot', type='text'), Field('student_id', type='reference auth_user'),\
-           Field('problem_id', type='reference problem'), Field('author_id', type='reference auth_user'), Field('status', requires=IS_IN_SET(['open', 'closed']), default='open'),\
-                 Field('posted_at', type='datetime'))
+    db.define_table('problem_topic', 
+        Field('problem_id', type='reference problem'), 
+        Field('topic_id', type='reference topic'))
 
-     db.define_table('comment', Field('message',type='text'), Field('discussion_id', type='reference discussion'), Field('author_id', type='reference auth_user'),\
-          Field('posted_at', type='datetime'))
-     
-     db.define_table('comment_like', Field('comment_id', type='reference comment'), Field('discussion_id', type='reference discussion'), Field('liked_by', type='reference auth_user'),\
-           Field('liked_at', type='datetime'), redefine=True)
+    db.define_table('student_workspace', 
+        Field('problem_id', type='reference problem'), 
+        Field('student_id', type='reference auth_user'), 
+        Field('content', type='text'), 
+        Field('attempt_left', type='integer'), 
+        Field('updated_at', type='datetime', default=datetime.datetime.utcnow()), redefine=True)
+    
+    db.define_table('submission', 
+        Field('problem_id', type='reference problem'), 
+        Field('student_id', type='reference auth_user'),
+        Field('content', type='text'), 
+        Field('submitted_at', type='datetime', default=datetime.datetime.utcnow()),
+        Field('submission_category', type='integer', default=1), 
+        Field('attempt', type='integer'), redefine=True)
 
-     db.commit()
+    db.define_table('submission_verdict', 
+        Field('submission_id', type='reference submission'), 
+        Field('verdict'), Field('score', type='double'),
+        Field('evaluated_at', type='datetime'))
+
+    db.define_table('notification', 
+        Field('message', type='text'), 
+        Field('recipients', type='list:reference auth_user'), 
+        Field('generated_at', type='datetime'),
+        Field('expire_at', type='datetime'), 
+        Field('type'), 
+        Field('type_id', type='integer'), redefine=True)
+
+    db.define_table('global_value', 
+        Field('variable'), 
+        Field('value'), redefine=True)
+
+    db.define_table('notification_queue', 
+        Field('notification_id', type='reference notification'), 
+        Field('user_id', type='reference auth_user'))
+
+    db.define_table('editor_notification_queue', 
+        Field('notification_id', type='reference notification'), 
+        Field('user_id', type='reference auth_user'))
+
+    db.define_table('discussion', 
+        Field('message', type='text'), 
+        Field('code_snapshot', type='text'), 
+        Field('student_id', type='reference auth_user'),
+        Field('problem_id', type='reference problem'), 
+        Field('author_id', type='reference auth_user'), 
+        Field('status', requires=IS_IN_SET(['open', 'closed']), default='open'),
+        Field('posted_at', type='datetime'))
+
+    db.define_table('comment', 
+        Field('message',type='text'), 
+        Field('discussion_id', type='reference discussion'), 
+        Field('author_id', type='reference auth_user'),
+        Field('posted_at', type='datetime'))
+
+    db.define_table('comment_like', 
+        Field('comment_id', type='reference comment'), 
+        Field('discussion_id', type='reference discussion'), 
+        Field('liked_by', type='reference auth_user'),
+        Field('liked_at', type='datetime'), redefine=True)
+
+    db.commit()
+
+#----------------------------------------
 
 create_tables()
+
--- src/py4web_app/student_workspace_controller.py
+++ src/py4web_app/student_workspace_controller.py
@@ -28,139 +28,146 @@ from .utils import create_notification, is_eligible_for_help
 @action('student_workspace_view/<student_id>/<problem_id>', method='GET')
 @action.uses(auth.user, 'student_workspace_view.html')
 def workspace_view(student_id, problem_id):
-        help_eligible = False
-        user_id = auth.get_user()['id']
-        grp = groups.get(user_id)
-        if 'teacher' in grp:
-                user_role = 'instructor'
-        elif 'ta' in grp:
-                user_role = 'ta'
-        elif 'student' in grp:
-                user_role = 'student'
-                if int(student_id) == user_id and is_eligible_for_help(user_id, problem_id):
-                        help_eligible = True
-        else:
-                redirect(URL('not_authorized'))
-        
-        ref = request.get_header('Referer')
-        if ref is not None:
-                ref = ref.split('/')
-        help_message_id = 0
-        problem, workspace, submissions, status, discussions = get_workspace_info(student_id, problem_id)
-        student_name = '{} {}'.format(
-                db.auth_user[student_id].first_name, db.auth_user[student_id].last_name)
-        
-        return dict(
-            problem=problem, 
-            workspace=workspace, 
-            time_interval=1000, 
-            submissions=submissions, 
-            help_eligible=help_eligible,
-            student_name=student_name, 
-            student_id=student_id, 
-            user_id=user_id,
-            help_message_id=help_message_id, 
-            status=status, 
-            user_role=user_role, 
-            discussions=discussions)
+    help_eligible = False
+    user_id = auth.get_user()['id']
+    grp = groups.get(user_id)
+    if 'teacher' in grp:
+        user_role = 'instructor'
+    elif 'ta' in grp:
+        user_role = 'ta'
+    elif 'student' in grp:
+        user_role = 'student'
+        if int(student_id) == user_id and is_eligible_for_help(user_id, problem_id):
+            help_eligible = True
+    else:
+        redirect(URL('not_authorized'))
+    
+    ref = request.get_header('Referer')
+    if ref is not None:
+            ref = ref.split('/')
+    help_message_id = 0
+    problem, workspace, submissions, status, discussions = get_workspace_info(student_id, problem_id)
+    student_name = '{} {}'.format(
+            db.auth_user[student_id].first_name, db.auth_user[student_id].last_name)
+    
+    return dict(
+        problem=problem, 
+        workspace=workspace, 
+        time_interval=1000, 
+        submissions=submissions, 
+        help_eligible=help_eligible,
+        student_name=student_name, 
+        student_id=student_id, 
+        user_id=user_id,
+        help_message_id=help_message_id, 
+        status=status, 
+        user_role=user_role, 
+        discussions=discussions)
 
+#--------------------------------------------------------------------
 def get_workspace_info(student_id, problem_id):
-        problem = db.problem[problem_id]
-        workspace = db((db.student_workspace.problem_id==problem_id) & (db.student_workspace.student_id==student_id)).select()
-        if workspace is None or len(workspace)==0:
-                db.student_workspace.insert(problem_id=problem_id, student_id=student_id, content=problem.code, attempt_left=problem.attempts)
-                db.commit()
-                workspace = db((db.student_workspace.problem_id==problem_id) & (db.student_workspace.student_id==student_id)).select().first()
-        else:
-                workspace = workspace.first()
+    problem = db.problem[problem_id]
+    workspace = db((db.student_workspace.problem_id==problem_id) & (db.student_workspace.student_id==student_id)).select()
+    if workspace is None or len(workspace)==0:
+            db.student_workspace.insert(problem_id=problem_id, student_id=student_id, content=problem.code, attempt_left=problem.attempts)
+            db.commit()
+            workspace = db((db.student_workspace.problem_id==problem_id) & (db.student_workspace.student_id==student_id)).select().first()
+    else:
+            workspace = workspace.first()
 
-        submissions = db((db.submission.student_id==student_id)&(db.submission.problem_id==problem_id)).select(db.submission.id, orderby=~db.submission.submitted_at)
-        # feedbacks = db.executesql("select f.id, u.first_name, u.last_name,  f.given_at from feedback f, auth_user u where f.problem_id="+str(problem_id)+" and f.given_for="+str(student_id)+" and f.submission_id is NULL and u.id=f.given_by order by f.given_at desc", as_dict=True)
-        if len(db((db.submission_verdict.verdict=="correct")&(db.submission_verdict.submission_id==db.submission.id)&(db.submission.problem_id==problem_id)&(db.submission.student_id==student_id)).select())>0:
-                status = "Graded correct"
-        elif len(db((db.submission_verdict.submission_id==db.submission.id)&(db.submission.problem_id==problem_id)&(db.submission.student_id==student_id)).select())>0:
-                status = "Graded incorrect"
-        else:
-                status = "Not Graded"
-        
-        discussions = db((db.discussion.problem_id==problem_id)&(db.discussion.student_id==student_id)).select(orderby=~db.discussion.posted_at)
-        return problem, workspace, submissions, status, discussions
+    submissions = db((db.submission.student_id==student_id)&(db.submission.problem_id==problem_id)).select(db.submission.id, orderby=~db.submission.submitted_at)
+    # feedbacks = db.executesql("select f.id, u.first_name, u.last_name,  f.given_at from feedback f, auth_user u where f.problem_id="+str(problem_id)+" and f.given_for="+str(student_id)+" and f.submission_id is NULL and u.id=f.given_by order by f.given_at desc", as_dict=True)
+    if len(db((db.submission_verdict.verdict=="correct")&(db.submission_verdict.submission_id==db.submission.id)&(db.submission.problem_id==problem_id)&(db.submission.student_id==student_id)).select())>0:
+            status = "Graded correct"
+    elif len(db((db.submission_verdict.submission_id==db.submission.id)&(db.submission.problem_id==problem_id)&(db.submission.student_id==student_id)).select())>0:
+            status = "Graded incorrect"
+    else:
+            status = "Not Graded"
+    
+    discussions = db((db.discussion.problem_id==problem_id)&(db.discussion.student_id==student_id)).select(orderby=~db.discussion.posted_at)
+    return problem, workspace, submissions, status, discussions
 
 
+#--------------------------------------------------------------------
 @action('get_student_code/<workspace_id>', method='GET')
 @action.uses(auth.user)
 def get_student_code(workspace_id):
-        user_id = auth.get_user()['id']
-        ws = db.student_workspace[workspace_id].as_dict()
-        if 'teacher' in groups.get(user_id):
-                return json.dumps(ws, default=str)
-        if 'student' in groups.get(user_id) and user_id == ws['student_id']:
-                return json.dumps(ws, default=str)
-        redirect(URL('not_authorized'))
+    user_id = auth.get_user()['id']
+    ws = db.student_workspace[workspace_id].as_dict()
+    if 'teacher' in groups.get(user_id):
+            return json.dumps(ws, default=str)
+    if 'student' in groups.get(user_id) and user_id == ws['student_id']:
+            return json.dumps(ws, default=str)
+    redirect(URL('not_authorized'))
 
 
+#--------------------------------------------------------------------
 @action('save_help_request', method='POST')
 @action.uses(auth.user)
 def save_help_request():
-        user_id = auth.get_user()['id']
-        if 'student' not in groups.get(user_id):
-                return "Unauthorized access"
-        # print(request.GET.keys())
-        message = request.POST['message']
-        problem_id = request.POST['problem_id']
-        # print(message, problem_id)
-        problem = db.problem[problem_id]
-        db.help_queue.insert(student_id=user_id, problem_id=problem_id, message=message, asked_at=datetime.datetime.utcnow())
-        db.commit()
-        create_notification("New help request recieved.", \
-                recipients=[ user['id'] for user in db(db.auth_user).select('id') if 'teacher' in groups.get(user['id'])], \
-                        expire_at=problem.deadline)
-        return "Your help request will be looked at soon."
+    user_id = auth.get_user()['id']
+    if 'student' not in groups.get(user_id):
+            return "Unauthorized access"
+    # print(request.GET.keys())
+    message = request.POST['message']
+    problem_id = request.POST['problem_id']
+    # print(message, problem_id)
+    problem = db.problem[problem_id]
+    db.help_queue.insert(student_id=user_id, problem_id=problem_id, message=message, asked_at=datetime.datetime.utcnow())
+    db.commit()
+    create_notification("New help request recieved.", \
+            recipients=[ user['id'] for user in db(db.auth_user).select('id') if 'teacher' in groups.get(user['id'])], \
+                    expire_at=problem.deadline)
+    return "Your help request will be looked at soon."
 
 
+#--------------------------------------------------------------------
 @action('save_discussion', method='GET')
 @action.uses(auth.user)
 def save_discussion():
-        user_id = auth.get_user()['id']
-        student_id = int(request.query.get('student_id'))
-        message = request.query.get('message')
-        problem_id = int(request.query.get('problem_id'))
-        code_snapshot = request.query.get('code_snapshot')
-        db.discussion.insert(message=message, student_id=student_id, author_id=user_id, code_snapshot=code_snapshot,\
-                 problem_id=problem_id, posted_at=datetime.datetime.utcnow())
-        db.commit()
+    user_id = auth.get_user()['id']
+    student_id = int(request.query.get('student_id'))
+    message = request.query.get('message')
+    problem_id = int(request.query.get('problem_id'))
+    code_snapshot = request.query.get('code_snapshot')
+    db.discussion.insert(message=message, student_id=student_id, author_id=user_id, code_snapshot=code_snapshot,\
+             problem_id=problem_id, posted_at=datetime.datetime.utcnow())
+    db.commit()
 
+#--------------------------------------------------------------------
 @action('save_comment', method='GET')
 @action.uses(auth.user)
 def save_comment():
-        user_id = auth.get_user()['id']
-        message = request.query.get('message')
-        discussion_id = int(request.query.get('discussion_id'))
-        db.comment.insert(message=message, discussion_id=discussion_id, author_id = user_id, posted_at=datetime.datetime.utcnow())
-        db.commit()
+    user_id = auth.get_user()['id']
+    message = request.query.get('message')
+    discussion_id = int(request.query.get('discussion_id'))
+    db.comment.insert(message=message, discussion_id=discussion_id, author_id = user_id, posted_at=datetime.datetime.utcnow())
+    db.commit()
 
+#--------------------------------------------------------------------
 @action('save_comment_like', method='GET')
 @action.uses(auth.user)
 def save_comment_like():
-        user_id = auth.get_user()['id']
-        
-        comment_id = int(request.query.get('comment_id'))
-        
-        if db((db.comment_like.comment_id==comment_id)&(db.comment_like.liked_by==user_id)).count()>0:
-                db(db.comment_like.comment_id==comment_id).delete()
-        else:
-                db.comment_like.insert(comment_id=comment_id, liked_by=user_id, liked_at=datetime.datetime.utcnow())
-        db.commit()
+    user_id = auth.get_user()['id']
+    
+    comment_id = int(request.query.get('comment_id'))
+    
+    if db((db.comment_like.comment_id==comment_id)&(db.comment_like.liked_by==user_id)).count()>0:
+        db(db.comment_like.comment_id==comment_id).delete()
+    else:
+        db.comment_like.insert(comment_id=comment_id, liked_by=user_id, liked_at=datetime.datetime.utcnow())
+    db.commit()
 
+#--------------------------------------------------------------------
 @action('save_discussion_like', method='GET')
 @action.uses(auth.user)
 def save_discussion_like():
-        user_id = auth.get_user()['id']
-        
-        discussion_id = int(request.query.get('discussion_id'))
+    user_id = auth.get_user()['id']
+    
+    discussion_id = int(request.query.get('discussion_id'))
 
-        if db((db.comment_like.discussion_id==discussion_id)&(db.comment_like.liked_by==user_id)).count()>0:
-                db(db.comment_like.discussion_id==discussion_id).delete()
-        else:
-                db.comment_like.insert(discussion_id=discussion_id, liked_by=user_id, liked_at=datetime.datetime.utcnow())
-        db.commit()
+    if db((db.comment_like.discussion_id==discussion_id)&(db.comment_like.liked_by==user_id)).count()>0:
+        db(db.comment_like.discussion_id==discussion_id).delete()
+    else:
+        db.comment_like.insert(discussion_id=discussion_id, liked_by=user_id, liked_at=datetime.datetime.utcnow())
+    db.commit()
